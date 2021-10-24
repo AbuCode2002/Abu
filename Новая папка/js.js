0eeb5247dc11d8233d12563012de24509cf065c5
AbuCode2002 <93094552+AbuCode2002@users.noreply.github.com>
2021-10-25 01:56:03 +0600

    Update js.js

--- Новая папка/js.js
+++ Новая папка/js.js
@@ -2,7 +2,7 @@ function validate() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     if (username == "admin" && password == "user") {
-        window.open("index.html");
+        window.open("index1.html");
     }
     else {
         alert("Неправильный Логин или Пароль!");
